95658b355e599407f0f1ef0c35b8b9466a9ee9d0
Ashfaqueghulamfareed <165644175+Ashfaqueghulamfareed@users.noreply.github.com>
2024-05-10 09:37:12 +0500

    Add files via upload

---
+++ vowel and consonent.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char d;
+	cin>> d;
+	if(d=='a'||d=='e'||d=='i'||d=='o'||d=='u')
+	{
+		cout<<"vowel";
+	}
+	
+	else
+	cout<<"consonent ";
+	
+	
+	
+	
+	
+}
